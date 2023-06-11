7339c49ee3763a910d6cab80e63142b7616fb00f
Radmir <RaRDautov@stud.kpfu.ru>
2023-06-11 21:06:33 +0300

    Initial commit

---
+++ Di/Model.cs
@@ -0,0 +1,30 @@
+﻿namespace Di;
+
+public enum LifeTime
+{
+    Singleton,
+    Scoped,
+    Transient
+}
+public abstract class ServiceDescriptor
+{
+    public required Type ServiceType { get; init; }
+    public LifeTime LifeTime { get; init; }
+}
+
+interface IContainerBuilder
+{
+    void Register(ServiceDescriptor descriptor);
+
+    IContainer Build();
+}
+
+interface IContainer
+{
+    IScope CreateScope();
+}
+
+interface IScope
+{
+    object Resolve(Type service);
+}
